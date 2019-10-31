d136f14106c6ca08d9745bb6416a29ab75b1ac70
meiyujack <meiyujack@msn.cn>
2019-10-31 17:19:54 +0800

    加入主页

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask,url_for
+from flask import Flask,url_for,render_template
 app=Flask(__name__)
 
+name='meiyujack'
+movies=[
+    {'title':'龙猫','year':'1988'},
+    {'title':'死亡诗社','year':'1989'},
+    {'title':'完美的世界','year':'1993'},
+    {'title':'这个杀手不太冷','year':'1994'},
+    {'title':'麻将','year':'1996'},
+    {'title':'燕尾蝶','year':'1996'},
+    {'title':'喜剧之王','year':'1999'},
+    {'title':'机器人总动员','year':'2008'},
+]
 
 @app.route('/')
-@app.route('/index')
+def index():
+    return render_template('index.html',name=name,movies=movies)
+
 @app.route('/home')
 def hello():
     return '欢迎来到我的电影菜单！'+'<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
