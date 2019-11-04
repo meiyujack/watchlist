48d0d9afdd790d21e44848a2e4d609ccea57e7e2
meiyujack <meiyujack@msn.cn>
2019-11-04 18:23:51 +0800

    用Flask-Login加入了用户认证

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import os,click
 
 from flask import Flask,url_for,render_template,redirect,request,flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import LoginManager,UserMixin,login_user,login_required,logout_user,current_user
 
 from models.movie import Movie
-
-
-
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(app.root_path,'data.db')
@@ -14,6 +13,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SECRET_KEY']='freedom'
 
 db=SQLAlchemy(app)
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+login_manager.login_message='请登录！'
 
 @app.cli.command()
 @click.option('--drop',is_flag=True,help='Create after drop.')
@@ -47,11 +49,72 @@ def forge():
         db.session.add(movie)
 
     db.session.commit()
+    click.echo('数据已导入。')
+
+@app.cli.command()
+@click.option('--username',prompt=True,help='设置用户名用来登陆')
+@click.option('--password',prompt=True,hide_input=True,confirmation_prompt=True,help='设置密码用来登陆')
+def admin(username,password):
+    """创建用户"""
+    db.create_all()
+
+    user=User.query.first()
+    if user is not None:
+        click.echo('更新用户中......')
+        user.username=username
+        user.set_password(password)
+    else:
+        click.echo('创建用户中......')
+        user=User(username=username,name='Admin')
+        user.set_password(password)
+        db.session.add(user)
+
+    db.session.commit()
     click.echo('完成。')
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    user=User.query.get(int(user_id))
+    return user
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+
+        if not username or not password:
+            flash('非法输入!')
+            return redirect(url_for('login'))
+
+        user=User.query.first()
+        if username==user.username and user.validate_password(password):
+            login_user(user)
+            flash('登陆成功！')
+            return redirect(url_for('index'))
+
+        flash('无效用户名或密码。')
+        return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('拜拜~')
+    return redirect(url_for('index'))
+
+class User(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(20))
+    username=db.Column(db.String(20))
+    password_hash=db.Column(db.String(128))
+
+    def set_password(self,password):
+        self.password_hash=generate_password_hash(password)
+
+    def validate_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
 class Movie(db.Model):
     id=db.Column(db.Integer,primary_key=True)
@@ -64,6 +127,8 @@ class Movie(db.Model):
 @app.route('/',methods=['GET','POST'])
 def index():
     if request.method=='POST':
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
         title=request.form.get('title')
         year=request.form.get('year')
         if not title or not year or len(year)!=4 or len(title)>60:
@@ -85,10 +150,12 @@ def index():
     # return render_template('index.html',movies=movies)
 
 @app.route('/add')
-def reg():
+@login_required
+def add():
     return render_template('form.html')
 
 @app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+@login_required
 def edit(movie_id):
     movie=Movie.query.get_or_404(movie_id)
 
@@ -109,6 +176,7 @@ def edit(movie_id):
     return render_template('edit.html',movie=movie)
 
 @app.route('/movie/delete/<int:movie_id>',methods=['POST'])
+@login_required
 def delete(movie_id):
     movie=Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
@@ -123,6 +191,23 @@ def hello():
 @app.route('/user/<name>')
 def user_page(name):
     return 'User:{} page '.format(name)
+
+@app.route('/settings',methods=['GET','POST'])
+@login_required
+def settings():
+    if request.method=='POST':
+        name=request.form['name']
+
+        if not name or len(name)>20:
+            flash('非法输入！')
+            return redirect(url_for('settings'))
+
+        current_user.name=name
+        db.session.commit()
+        flash('设置已更新。')
+        return redirect(url_for('index'))
+
+    return render_template('settings.html')
 
 @app.route('/test')
 def test_url_for():
